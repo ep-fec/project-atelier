c05bb2bbb77a048979d3cda0d4a80f1347c8f0df
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-08-11 22:49:39 -0500

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/client/src/index.jsx'),
+  output: {
+    path: path.join(__dirname, '/client/dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
