e7cb082e4b66f4c71cea93d612bc8e933e1fe688
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-08-18 13:35:06 -0500

    Initial widget skeleton

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -4,8 +4,20 @@ const Reviews = (props) => {
 
 
   return (
-    <div>
+    <div className="reviews main">
       Ratings & Reviews
+      <section className="reviews leftcol">
+        <div className="reviews rating-breakdown">Rating Breakdown</div>
+        <div className="reviews product-breakdown">Product Breakdown</div>
+      </section>
+      <section className="reviews-rightcol">
+        <div className="reviews sort-options">Sort Options</div>
+        <div className="reviews reviews-list">Reviews List
+          <div className="reviews individual-review">Individual Review</div>
+        </div>
+      </section>
+      <button className="reviews more-reviews">More Reviews</button>
+      <button className="reviews add-review">Add Review</button>
     </div>
   )
 }
