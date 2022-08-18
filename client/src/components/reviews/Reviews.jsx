d4f71cc9749e4442dee82112a23f5d591410e984
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-08-18 18:50:34 -0500

    Refine initial widget skeleton

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -4,21 +4,25 @@ const Reviews = (props) => {
 
 
   return (
-    <div className="reviews main">
-      Ratings & Reviews
+    <section className="reviews-container">
+      <h2>RATINGS & REVIEWS </h2>
+      <br/><br/>
+
       <section className="reviews leftcol">
         <div className="reviews rating-breakdown">Rating Breakdown</div>
         <div className="reviews product-breakdown">Product Breakdown</div>
       </section>
-      <section className="reviews-rightcol">
-        <div className="reviews sort-options">Sort Options</div>
-        <div className="reviews reviews-list">Reviews List
-          <div className="reviews individual-review">Individual Review</div>
+
+      <section className="reviews rightcol">
+        <div className="reviews sort-options">**Sort Options**</div>
+        <div className="reviews reviews-list">Reviews List Box
+          <div className="reviews individual-review">----Individual Reviews-----</div>
         </div>
+        <br/>
+        <button className="reviews more-reviews reviewsbutton">MORE REVIEWS</button>
+        <button className="reviews add-review reviewsbutton">ADD A REVIEW +</button>
       </section>
-      <button className="reviews more-reviews">More Reviews</button>
-      <button className="reviews add-review">Add Review</button>
-    </div>
+    </section>
   )
 }
 
