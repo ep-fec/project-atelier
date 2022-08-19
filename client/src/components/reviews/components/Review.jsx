041c546fa48cc6c8d1d2f156d5439887e9c1aab7
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-08-19 10:47:51 -0500

    Initial CSS for indivial-review

--- client/src/components/reviews/components/Review.jsx
+++ client/src/components/reviews/components/Review.jsx
@@ -6,16 +6,16 @@ const Review = (props) => {
     <div className="reviews ind-review">
 
       <section className="reviews ind-review-heading">
-        <span className="reviews ind-review-stars">Star Rating</span>
-        <span className="reviews ind-review-username">Reviewer Name</span>
+        <span className="reviews ind-review-stars">Star Rating Placeholder☆☆</span>
         <span className="reviews ind-review-date">Date of Review</span>
+        <span className="reviews ind-review-username">Reviewer Name,</span>
       </section>
 
       <section className="reviews ind-review-body">
         <h3 className="reviews ind-review-title">REVIEW TITLE</h3>
-        <span className="reviews ind-review-summary">Summary</span>
-        <p className="reviews ind-review-content">Review Content</p>
-        <span className="reviews ind-review-recommendation">Recommendation</span>
+        <span className="reviews ind-review-summary">Summary of the review...</span>
+        <p className="reviews ind-review-content">Review Content and body and many words and some other things that will be in the reivew and more words and just some text and other things.</p>
+        <span className="reviews ind-review-recommendation">√ Recommendation</span>
       </section>
 
       <section className="reviews ind-review-footer">
@@ -23,9 +23,10 @@ const Review = (props) => {
           <h4 className="reviews response-title">RESPONSE:</h4>
           <span className="reviews response-body">Some response body.</span>
         </div>
-        <span className="reviews ind-review-helpful">Helpful?</span>
+        <span className="reviews ind-review-helpful">Helpful? </span>
+        <span className="reviews ind-review-report">| Report</span>
+        <hr/>
       </section>
-      <hr/>
     </div>
   )
 
--- client/src/components/reviews/components/Sort.jsx
+++ client/src/components/reviews/components/Sort.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Sort = (props) => {
 
   return (
-    <div className="reviews sort-options">-Sort Options-</div>
+    <div className="reviews sort-options">-Sort Options Placeholder-</div>
   )
 
 }
