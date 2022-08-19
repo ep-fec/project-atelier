c7efc212435502c93a6d3a068605b630c56f0e01
William Kent <kentwl1876@gmail.com>
2022-08-18 19:07:19 -0800

    Set up of functions for api requests (may need to adjust the post and put later on) and set up example.env with a key and some descriptions.

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const express = require ('express');
 const path = require('path');
 const app =  express();
 const port = process.env.PORT;
+const request = require('./helpers/request.js');
+// TODO: request functions for post and put may need to be adjusted
+// Don't put the first / in the endpoint for requests
 
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.use(express.json());
