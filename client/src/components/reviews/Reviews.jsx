15dc5b8a5790af5ee1e43807142b6bf74e1c726c
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-08-22 21:11:45 -0500

    Load reviews from API

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -6,8 +6,26 @@ import Ratings from './components/Ratings.jsx';
 import Sort from './components/Sort.jsx';
 import NewReview from './components/NewReview.jsx';
 import data from './components/sampleData.js';
+import axios from 'axios';
 
 const Reviews = (props) => {
+
+  const getReviews = () => {
+    axios.get(`/reviews?product_id=${props.currentProduct}&count=50`)
+      .then((res) => setReviews(res.data))
+      .catch((err) => console.log(err));
+  }
+
+  let [reviews, setReviews] = useState({results: []});
+
+  useEffect(() => {
+    getReviews();
+  }, [props.currentProduct]);
+
+
+
+
+
 
   return (
     <section className="reviews-container">
@@ -21,7 +39,7 @@ const Reviews = (props) => {
 
       <section className="reviews rightcol">
         <Sort />
-        <List data={data}/>
+        <List reviews={reviews}/>
         <br/>
         <button className="reviews more-reviews reviewsbutton">MORE REVIEWS</button>
         <button className="reviews add-review reviewsbutton">ADD A REVIEW +</button>
--- client/src/components/reviews/components/List.jsx
+++ client/src/components/reviews/components/List.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Review from './Review.jsx';
 
 const List = (props) => {
 
   return (
     <div className="reviews reviews-list">
-      <Review />
+      {props.reviews.results.length ?
+      props.reviews.results.map((review) => {
+        return (<Review data={review} key={review.review_id}/>)
+      }) : 'Be the first to leave a review!'}
     </div>
   )
-
 }
 
 export default List;
