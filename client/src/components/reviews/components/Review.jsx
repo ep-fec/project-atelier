14d5c4ffc18e0c53d5bb1d5d462053b2478702a9
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-08-22 22:32:46 -0500

    Add conditional to review response. Display review helpfulness rating

--- client/src/components/reviews/components/Review.jsx
+++ client/src/components/reviews/components/Review.jsx
@@ -19,19 +19,24 @@ const Review = (props) => {
 
       <section className="reviews ind-review-body">
         <h3 className="reviews ind-review-summary">{props.data.summary}</h3>
-        {/* <span className="reviews ind-review-summary">{props.data.summary}</span> */}
         <p className="reviews ind-review-content">{props.data.body}</p>
-        <span className="reviews ind-review-recommendation">√ Recommendation</span>
+        <span className="reviews ind-review-recommendation">
+          {props.data.recommend ? '✔️ I recommend this product' : null}
+        </span>
+        {props.data.response ?
+        <div className="reviews ind-review-response">
+          <h4 className="reviews response-title">Response from seller:</h4>
+          <span className="reviews response-body">{props.data.response}</span>
+        </div>
+        : null}
       </section>
 
       <section className="reviews ind-review-footer">
-        <div className="reviews ind-review-response">
-          <h4 className="reviews response-title">RESPONSE:</h4>
-          <span className="reviews response-body">Some response body.</span>
-        </div>
         <span className="reviews ind-review-helpful">Helpful? </span>
-        <span className="reviews ind-review-report">| Report</span>
-        <hr/>
+        <span className="reviews ind-review-helpfulYes">Yes</span>
+        <span className="reviews ind-review-helpfulness"> ({props.data.helpfulness}) | </span>
+        <span className="reviews ind-review-report">Report</span>
+        <br/><hr/>
       </section>
     </div>
   )
