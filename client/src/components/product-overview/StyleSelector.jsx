d6bd7b83222325456dad886ffd6bd26ce6a5bc2b
huchchas <hsuchchas@gmail.com>
2022-08-26 12:11:24 -0400

    Selected thumbnail shows red border

--- client/src/components/product-overview/StyleSelector.jsx
+++ client/src/components/product-overview/StyleSelector.jsx
@@ -2,14 +2,34 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 export default function StyleSelector({allStyles, selectedStyle, setSelectedStyle}) {
+  const [checkmarkStatus, setCheckmarkStatus] = useState([]);
+
   useEffect(() => {
     if (allStyles.length !== 0) {
       setSelectedStyle(allStyles[0]);
+      setCheckmarkStatus(setCheckmarkToTrue(0));
     }
   }, [allStyles])
 
   const handleStyleSelectClick = (e) => {
     setSelectedStyle(allStyles[e.target.id]);
+    setCheckmarkStatus(prevState => setCheckmarkToTrue(e.target.id));
+  }
+
+  // Helper function
+  const setAllCheckmarkToFalse = () => {
+    let array = [];
+    allStyles.map(style => {
+      array.push(false);
+    })
+    return array;
+  }
+
+  // Helper function
+  const setCheckmarkToTrue = (target) => {
+    let array = setAllCheckmarkToFalse();
+    array[target] = true;
+    return array;
   }
 
   return (
@@ -20,15 +40,19 @@ export default function StyleSelector({allStyles, selectedStyle, setSelectedStyl
 
       <div className='styleThumbnailsGrid'>
         {allStyles.length !== 0 && (
-          allStyles.map((style, index) => (
-            <img className='thumbnail'
-              src={style.photos[0].thumbnail_url}
-              key={index}
-              onClick={handleStyleSelectClick}
-              name={style.name}
-              id={index}
-            />
-          ))
+          allStyles.map((style, index) => {
+            let showCheckmark = checkmarkStatus[index] ? 'styleThumbnailCheckmark' : null;
+
+            return (
+              <img className={`thumbnail ${showCheckmark}`}
+                src={style.photos[0].thumbnail_url}
+                key={index}
+                onClick={handleStyleSelectClick}
+                name={style.name}
+                id={index}
+              />
+            )
+          })
         )}
       </div>
     </div>
