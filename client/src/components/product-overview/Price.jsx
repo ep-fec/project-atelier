e4f0c17b8756afe40d3f6fdfe31f7fdf39850f2a
huchchas <hsuchchas@gmail.com>
2022-08-29 21:50:09 -0400

    Minor css changes for overview

--- client/src/components/product-overview/Price.jsx
+++ client/src/components/product-overview/Price.jsx
@@ -6,17 +6,17 @@ export default function Price({selectedStyle}) {
       {selectedStyle.sale_price !== null && (
         <>
           <div className='salePrice'>
-            {selectedStyle.sale_price}
+            ${selectedStyle.sale_price}
           </div>
           <div className='originalPrice'>
-            {selectedStyle.original_price}
+            ${selectedStyle.original_price}
           </div>
         </>
       )}
 
       {selectedStyle.sale_price === null && (
         <div className='price'>
-          {selectedStyle.original_price}
+          ${selectedStyle.original_price}
         </div>
       )}
     </>
