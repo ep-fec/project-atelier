de52daa53b5c0ff5c938a6b5024205c9f04f55ae
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-08-31 08:15:22 -0500

    Calculate and display percent of recommendations

--- client/src/components/reviews/components/Ratings.jsx
+++ client/src/components/reviews/components/Ratings.jsx
@@ -4,6 +4,8 @@ const Ratings = (props) => {
   let data = props.reviews;
   let [totalReviews, setTotalReviews] = useState(0);
   let [totalRating, setTotalRating] = useState('');
+  let [recommendations, setRecommendations] = useState(0);
+  let recCounter = 0;
 
   const ratings = {
     '1': 0,
@@ -22,14 +24,15 @@ const Ratings = (props) => {
   useEffect(() => {
     if (totalReviews) {
       setTotalRating(calcRating().toFixed(1));
-      console.log(ratings);
     }
   }, [totalReviews])
 
   function calcRating() {
     data.forEach((review) => {
       ratings[review.rating]++;
+      review.recommend ? recCounter++ : null;
     });
+    setRecommendations(Math.ceil((recCounter / totalReviews) * 100));
     let scoreTotal = (ratings[1] * 1) + (ratings[2] * 2) + (ratings[3] * 3) + (ratings[4] * 4) + (ratings[5] * 5);
     return scoreTotal / totalReviews;
   }
@@ -42,6 +45,7 @@ const Ratings = (props) => {
         <h1 className="reviews total-rating">{totalRating} </h1>
         <span className="reviews stars rating-stars" style={{'--rating':  totalRating}}></span>
       </div>
+      <span className="reviews recommend-product">{recommendations}% of reviews recommend this product</span>
 
     </div>
   )
