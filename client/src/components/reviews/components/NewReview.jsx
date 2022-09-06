403acb78699c85b6f4f39e316b384735aa7c4d43
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-09-06 16:13:38 -0500

    Initial new-review product recommendation layout

--- client/src/components/reviews/components/NewReview.jsx
+++ client/src/components/reviews/components/NewReview.jsx
@@ -8,7 +8,6 @@ const NewReview = ({productInfo, productMeta}) => {
     const [selected, setSelected] = useState({1:false, 2:false, 3:false, 4:false, 5:false});
     
     const handleStarClick = (star) => {
-        console.log('hi');
         star === 1 ? setSelected({1: true, 2: false, 3: false, 4: false, 5: false })
         : star === 2 ? setSelected({ 1: true, 2: true, 3: false, 4: false, 5: false })
         : star === 3 ? setSelected({ 1: true, 2: true, 3: true, 4: false, 5: false })
@@ -20,11 +19,11 @@ const NewReview = ({productInfo, productMeta}) => {
     return (
         <div className="reviews new-review-component">
             <div className="new-review-header">
-                <h1 className="reviews-logo">• WRITE YOUR REVIEW •</h1>
-                <h2>About the {productInfo.name}</h2>
+                <h1 className="reviews-logo">WRITE YOUR REVIEW</h1>
+                <h2>About the <span className="new-review-product-name">{productInfo.name}</span></h2>
             </div>
             <div className="new-review-body">
-                <span className="new-review-overall-rating"> Overall Rating: </span>
+                <span className="new-review-category"><span className="new-review-required">* </span>Overall Rating: </span>
                 <span className="reviews new-review-stars">
                     <span className={selected[5] ? 'new-review-stars-selected' : null}
                     onClick={() => handleStarClick(5)}>☆</span>
@@ -37,6 +36,19 @@ const NewReview = ({productInfo, productMeta}) => {
                     <span className={selected[1] ? 'new-review-stars-selected' : null}
                     onClick={() => handleStarClick(1)}>☆</span>
                 </span>
+                <div className="new-review-category new-review-recommend">
+                    <span><span className="new-review-required">* </span>Do you recommend this product?</span>
+                    <div className="new-review-radio">
+                        <label>
+                            <input type="radio" name="no" value="no"/>
+                            Yes
+                        </label>
+                        <label>
+                            <input type="radio" name="no" value="no" />
+                            No
+                        </label>
+                    </div>
+                </div>
             </div>
         </div>
     );
