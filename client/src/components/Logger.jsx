b962013678ffaee89b05a6105b066c15f6d428bf
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-09-12 13:48:06 -0500

    Fix incomplete code line

--- client/src/components/Logger.jsx
+++ client/src/components/Logger.jsx
@@ -1,38 +1,38 @@
 import React from 'react';
 
 const withLogger = (WrappedComponent) => {
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-      this.handleMouseClick = this.handleMouseClick.bind(this);
-      this.state = {
-        element: '',
-        widget: props.widget,
-        time: '',
+    return class extends React.Component {
+      constructor(props) {
+        super(props);
+        this.handleMouseClick = this.handleMouseClick.bind(this);
+        this.state = {
+          element: '',
+          widget: props.widget,
+          time: '',
+        }
       }
-    }
 
-  handleMouseClick(e) {
-    console.log(e.target.outerHTML)
-    let date = new Date();
-    let time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
-    let element = e.target.outerHTML;
-    this.setState({
-      element,
-      time
-    });
+    handleMouseClick(e) {
+      console.log(e.target.outerHTML)
+      let date = new Date();
+      let time = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+      let element = e.target.outerHTML;
+      this.setState({
+        element,
+        time
+      });
+    };
 
+    render() {
+      return (
+        <div onClick={(e) => this.handleMouseClick(e)}>
+          <WrappedComponent {...this.props}/>
+        </div>
+      );
+    };
   };
-
-  render() {
-    return (
-      <div onClick={(e) => this.handleMouseClick(e)}>
-        <WrappedComponent {...this.props}/>
-      </div>
-    );
-  };
-};
 }
+
 export default withLogger;
 
 /*
