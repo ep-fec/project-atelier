6dc33f0dbc596cf150ccb9b571d68e54012869b1
Abdiel Sanchez <gaud.abdiel@gmail.com>
2022-09-13 17:32:13 -0500

    Remove comment with business requirements

--- client/src/components/Logger.jsx
+++ client/src/components/Logger.jsx
@@ -52,14 +52,3 @@ const withLogger = (WrappedComponent) => {
 }
 
 export default withLogger;
-
-/*
-The click tracking will not be through any 3rd party analytics platform. All will be done in-house. To track interactions, each click on the web page should be recorded, as well as metadata associated with that click.
-
-For each click on the page, we need to capture the following:
-
-Element of the page which was clicked
-Time of click
-Module clicke
-
-*/
